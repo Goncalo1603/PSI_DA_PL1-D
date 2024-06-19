2659b83f3b0c10735adc061011728637dcdd7f8b
isabel-costa <2231698@my.ipleiria.pt>
2024-06-19 12:44:18 +0100

    Isabel - Adicionei as funções para guardar na base de dados no maincontroller

--- Projeto/controller/maincontroller.cs
+++ Projeto/controller/maincontroller.cs
@@ -10,6 +10,91 @@ namespace Projeto.controller
 {
     internal class maincontroller
     {
+        private ExtraController _extracontroller;
+        private pratocontroller _pratocontroller;
+        private ReservaController _reservacontroller;
 
+        public maincontroller()
+        {
+
+            _extracontroller = new ExtraController();
+            _pratocontroller = new pratocontroller();
+            _reservacontroller = new ReservaController();
+        }
+
+        public void AddPrato(string descricao, string tipo, string ativo)
+        {
+            prato novoPrato = new prato
+            {
+                descricao = descricao,
+                tipo = tipo,
+                ativo = ativo
+            };
+
+            _pratocontroller.AddPrato(novoPrato);
+        }
+
+        public void UpdatePrato(int id, string descricao, string tipo, string ativo)
+        {
+            prato pratoAtualizado = new prato
+            {
+                id = id,
+                descricao = descricao,
+                tipo = tipo,
+                ativo = ativo
+            };
+
+            _pratocontroller.UpdatePrato(pratoAtualizado);
+        }
+
+        public void PratoExtra(int id, string descricao, string tipo, string ativo)
+        {
+            _pratocontroller.DeletePrato(id);
+        }
+
+        public void AddExtra(string descricao, float preco, string ativo)
+        {
+            extra novoExtra = new extra
+            {
+                descricao = descricao,
+                preco = preco,
+                ativo = ativo
+            };
+
+            _extracontroller.AddExtra(novoExtra);
+        }
+
+        public void UpdateExtra(int id, string descricao, float preco, string ativo)
+        {
+            extra extraAtualizado = new extra
+            {
+                id = id,
+                descricao = descricao,
+                preco = preco,
+                ativo = ativo
+            };
+
+            _extracontroller.UpdateExtra(extraAtualizado);
+        }
+
+        public void DeleteExtra(int id, string descricao, float preco, string ativo)
+        {
+            _extracontroller.DeleteExtra(id);
+        }
+
+        public List<prato> GetAllPratos()
+        {
+            return _pratocontroller.GetAllPratos();
+        }
+
+        public List<extra> GetAllExtras()
+        {
+            return _extracontroller.GetAllExtras();
+        }
+
+        public List<Reserva> GetAllReservas()
+        {
+            return _reservacontroller.GetAllReservas();
+        }
     }
 }
